79ae39de4cbd910e31e61b97e8b3d6d47ee84b97
sasidhar1123 <sasidharreddy516@gmail.com>
2025-06-15 09:36:57 +0530

    Delete model/rewriter.py

--- model/rewriter.py
+++
@@ -1,9 +0,0 @@
-# rewriter.py – Memory Compression System
-
-class MemoryRewriter:
-    def __init__(self):
-        pass
-
-    def compress(self, memory):
-        # Compress memory dict using basic rule
-        return {k: v for k, v in memory.items() if len(v) < 1000}
